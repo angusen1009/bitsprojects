48a706707df2d1574e9f0a065237e02063a6096e
Senthil <angusen@gmail.com>
2026-06-28 14:04:31 +0530

    first version

---
+++ first.java
@@ -0,0 +1,10 @@
+import java.io.*;
+public class test
+{
+public static void main(String args[]) throws Excpetion
+{
+System.out.println("Hellow Java");
+System.out.println("this is updated line");
+}
+}
+
